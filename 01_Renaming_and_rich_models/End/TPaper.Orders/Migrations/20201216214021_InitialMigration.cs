9fa58ac194e72d3f3ac31b008a58649bdfc36c73
Stas Lebedenko <>
2021-10-31 18:09:56 +0200

    Step_1_End_initial

---
+++ 01_Renaming_and_rich_models/End/TPaper.Orders/Migrations/20201216214021_InitialMigration.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TPaperOrders.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    EdiClientId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Inventory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Number = table.Column<int>(nullable: false),
+                    ProductId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventory", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    ExternalCode = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EdiOrder",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClientId = table.Column<int>(nullable: false),
+                    Quantity = table.Column<decimal>(nullable: false),
+                    ProductCode = table.Column<int>(nullable: false),
+                    Notes = table.Column<string>(nullable: true),
+                    DeliveryId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EdiOrder", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Delivery",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Number = table.Column<decimal>(nullable: false),
+                    ClientId = table.Column<int>(nullable: false),
+                    EdiOrderId = table.Column<int>(nullable: false),
+                    ProductCode = table.Column<int>(nullable: false),
+                    ProductId = table.Column<int>(nullable: false),
+                    Notes = table.Column<string>(nullable: true),
+                    EdiOrderId1 = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Delivery", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Delivery_EdiOrder_EdiOrderId1",
+                        column: x => x.EdiOrderId1,
+                        principalTable: "EdiOrder",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Client",
+                columns: new[] { "Id", "EdiClientId", "Name" },
+                values: new object[] { 1, 1, "Test client" });
+
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "Id", "ExternalCode", "Name" },
+                values: new object[] { 1, 1, "Sample" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Delivery_EdiOrderId1",
+                table: "Delivery",
+                column: "EdiOrderId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EdiOrder_DeliveryId",
+                table: "EdiOrder",
+                column: "DeliveryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EdiOrder_Delivery_DeliveryId",
+                table: "EdiOrder",
+                column: "DeliveryId",
+                principalTable: "Delivery",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Delivery_EdiOrder_EdiOrderId1",
+                table: "Delivery");
+
+            migrationBuilder.DropTable(
+                name: "Client");
+
+            migrationBuilder.DropTable(
+                name: "Inventory");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "EdiOrder");
+
+            migrationBuilder.DropTable(
+                name: "Delivery");
+        }
+    }
+}
---
+++ 01_Renaming_and_rich_models/End/TPaper.Orders/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TPaper.Orders;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace TPaper.Orders
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+
+            builder.Services.AddLogging(options =>
+            {
+                options.AddFilter("TPaperOrders", LogLevel.Information);
+            });
+
+            builder.Services.AddOptions<ProjectOptions>()
+                .Configure<IConfiguration>((settings, configuration) =>
+                {
+                    configuration.GetSection("ProjectOptions").Bind(settings);
+                });
+
+            string sqlString = Environment.GetEnvironmentVariable("SqlPaperString");
+            string sqlPassword = Environment.GetEnvironmentVariable("SqlPaperPassword");
+            string connectionString = new SqlConnectionStringBuilder(sqlString) { Password = sqlPassword }.ConnectionString;
+
+            builder.Services.AddDbContextPool<PaperDbContext>(options =>
+            {
+                if (!string.IsNullOrEmpty(connectionString))
+                {
+                    options.UseSqlServer(connectionString, providerOptions => providerOptions.EnableRetryOnFailure());
+                }
+            });
+
+            PaperDbContext.ExecuteMigrations(connectionString);
+        }
+    }
+}
